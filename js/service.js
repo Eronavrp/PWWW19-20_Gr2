6857bd08b9c9482666847a4d0826927fe546889d
Loreta Shala <loreta.shala@studentet.uni-pr.edu>
2019-12-29 04:04:20 +0100

    editimi i faqes services, futja e fotove dhe te dhenave

--- js/service.js
+++ js/service.js
@@ -2,7 +2,36 @@
 function HideContent(obj) {
     for (var i = 1; i < obj.length; i++) {
         document.getElementById([obj[0]]).style.display = 'flex';
-      document.getElementById([obj[i]]).style.display = 'none';
-      
+        document.getElementById([obj[i]]).style.display = 'none';
+
     }
-  }
+}
+
+function HideContent1(obj) {
+    
+      if(  document.getElementById(obj).style.display == 'none')
+      {document.getElementById(obj).style.display = 'block';}
+      else
+      {
+        document.getElementById(obj).style.display = 'none';
+      }
+       
+
+    
+}
+s
+// Filename: script.js
+function myFunction() {
+    var element = document.getElementById("map");
+    var xdesiredPosition = 930;
+    var elmnt = document.getElementById("w1");
+    var y = elmnt.scrollHeight;
+    
+    if (window.pageYOffset >= xdesiredPosition) {
+        element.style.cssText += "position: fixed; top: auto; bottom: 0px; right: 105px; left: auto;";
+      }
+
+    
+    else {
+        element.style.cssText += "position: relative; top: 40px; bottom: auto; right: auto; left: 20px;";
+    }}
