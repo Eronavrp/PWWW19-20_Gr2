a82d7664d7d1009bb6b36baa602a94be2935c910
VegimShala <vegim.shala@studentet.uni-pr.edu>
2019-12-30 03:00:00 +0100

    Places vazhdim

--- js/places.js
+++ js/places.js
@@ -18,7 +18,7 @@ function showDivs(n)
     if (slideIndex+n > divs.length-5 && n>0   ) {n=0; }
     else if (slideIndex+n < 1 && n<0) {n=0; }
   
-    if(n!=0)
+    if(n!=0 )
     {
         slideIndex = slideIndex + n;
         var temp;
@@ -29,8 +29,29 @@ function showDivs(n)
             if(n<0) {   divs[divs.length-(i+1)].innerHTML = divs[divs.length-(i+2)].innerHTML;   }
             else if(n>0) divs[i].innerHTML = divs[i+1].innerHTML;
         }
-        if(n>0) divs[divs.length-1].innerHTML = temp;
+        if(n>0) {divs[divs.length-1].innerHTML = temp;}
         else if(n<0) divs[0].innerHTML = temp;
     }
 }
 
+function showStory(n)
+{
+    var stories = document.getElementsByClassName("readMore");
+    for( i = 0; i< stories.length; i++){
+        stories[i].className = "readMore hidden";
+    }
+    stories[n].className = "readMore";
+    document.getElementById("main").style.height = "1300px";
+}
+
+function showNextPicture(place,index)
+{
+    var pictures = [
+        ["images/a1.jpg","images/a2.jpg","images/a3.jpg","images/a4.jpg"],
+        ["images/a5.jpg","images/a6.jpg","images/a7.jpg","images/a8.jpg"],
+        ["images/a9.jpg","images/a10.jpg","images/a11.jpg","images/a12.jpg"]];
+    var s = document.getElementsByClassName("miniSlideShow");
+    s[place].src = pictures[place][index];
+    
+    
+}
