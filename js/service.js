ee5728e021020f985b5f5b0b8ceb8f44549ac2fa
Loreta Shala <loreta.shala@studentet.uni-pr.edu>
2020-01-02 18:22:54 +0100

    Perfundimi i Sunny Hill Festival

--- js/service.js
+++ js/service.js
@@ -41,3 +41,17 @@ function myFunction() {
     else {
         element.style.cssText += "position: relative; top: 40px; bottom: auto; right: auto; left: 20px;";
     }}
+    
+    function allowDrop(ev) {
+      ev.preventDefault();
+    }
+    
+    function drag(ev) {
+      ev.dataTransfer.setData("text", ev.target.id);
+    }
+    
+    function drop(ev) {
+      ev.preventDefault();
+      var data = ev.dataTransfer.getData("text");
+      ev.target.appendChild(document.getElementById(data));
+    }
