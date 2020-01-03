23c0f20ba176890278fa3fe563e70c19f605dc33
VegimShala <vegim.shala@studentet.uni-pr.edu>
2020-01-03 01:49:38 +0100

    Dropdown finished

--- js/dropdown.js
+++ js/dropdown.js
@@ -2,6 +2,6 @@ $(window).on('load',function () {
     $('#drop').hover(function(){
       $('.dropdown-content').slideDown(500);
     },function(){
-      $('.dropdown-content').slideUp(500);
+      $('.dropdown-content').slideUp(1500);
     });
   });
