bd27e80ed7ee3cadd3a8139bc253ca24236aa261
VegimShala <vegim.shala@studentet.uni-pr.edu>
2020-01-03 01:42:07 +0100

    Dropdown fix, and places nav

--- js/dropdown.js
+++ js/dropdown.js
@@ -1,6 +1,6 @@
 $(window).on('load',function () {
     $('#drop').hover(function(){
-      $('.dropdown-content').slideDown(2500);
+      $('.dropdown-content').slideDown(500);
     },function(){
       $('.dropdown-content').slideUp(500);
     });
